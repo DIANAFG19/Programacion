89de8baad462bff89ab894fbdcb82dc0fb34e016
Diana <diana1310fg@gmail.com>
2018-08-07 13:08:04 -0500

    Algo hecho...

--- Introduccion/Introduccion/Program.cs
+++ Introduccion/Introduccion/Program.cs
@@ -8,58 +8,59 @@ namespace Introduccion
 {
     class Program
     {
+        public static int opcion = 0;
+
         static void Main(string[] args)
         {
-            //MENÚ DE INICIO.
-            Console.Write("\n\t\t\tBIENVENIDO\n\n \t\tMENÚ");
-            Console.Write("\n\t1.- Hola Mundo. \n\t2.- Saltos de Línea, tabulación, comentarios.");
-            Console.Write("\n\t3.- Recepción de variables. \n\t4.- Suma de dos números.\n\n");
-            Console.Write("SELECCIONE LA SESIÓN QUE DESEA CONSULTAR (1, 2, 3 o 4): ");
-            int opcion = Convert.ToInt16(Console.ReadLine());
+            do
+            {
+                //MENÚ DE INICIO.
+                Console.Write("\n\t\t\tBIENVENIDO\n\n \t\tMENÚ");
+                Console.Write("\n\t1.- Hola Mundo. \n\t2.- Saltos de Línea, tabulación, comentarios.");
+                Console.Write("\n\t3.- Recepción de variables. \n\t4.- Suma de dos números. \n\t5.- Salir.\n\n");
+                Console.Write("SELECCIONE LA SESIÓN QUE DESEA CONSULTAR (1, 2, 3 o 4) O SI DESEA SALIR (5): ");
+                int opcion = Convert.ToInt16(Console.ReadLine());
 
-            if(opcion == 1)
-            {
-                Console.Write("Bienvenido a la programación en C# desde consola.");
-                Console.WriteLine("Bienvenido a la programación en C# desde consola.");
-                Console.WriteLine("Saliendo... Press any key...");
-                Console.ReadKey();
-            }
-            else if (opcion == 2)
-            {
-                /*
-                 * Comentario a bloque.
-                 * 
-                 */
-                 // Comentario de línea.
+                if (opcion == 1)
+                {
+                    Console.Write("Bienvenido a la programación en C# desde consola.");
+                    Console.WriteLine("Bienvenido a la programación en C# desde consola.");
+                    Console.WriteLine("Saliendo... Press any key...");
+                }
+                else if (opcion == 2)
+                {
+                    /*
+                     * Comentario a bloque.
+                     * 
+                     */
+                    // Comentario de línea.
 
-                Console.WriteLine("Saliendo... Press any key...");
+                    Console.WriteLine("Saliendo... Press any key...");
+                }
+                else if (opcion == 3)
+                {
+                    Console.WriteLine("Escribe tu nombre: ");
+                    string nombre = Console.ReadLine();
+                    Console.WriteLine("Tu nombre es " + nombre);
+                    Console.WriteLine("Saliendo... Press any key...");
+                }
+                else if (opcion == 4)
+                {
+                    // Hacer una suma con dos números que ingrese el usuario.
+                    Console.Write("Escribe un número: ");
+                    int numero1 = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("Escribe un número: ");
+                    int numero2 = Convert.ToInt16(Console.ReadLine());
+                    int suma = numero1 + numero2;
+                    Console.WriteLine("La suma de " + numero1 + " + " + numero2 + " = " + suma);
+                    Console.WriteLine("Saliendo... Press any key...");
+                }
+                else
+                {
+                    Console.WriteLine("Opción incorrecta...");
+                }
                 Console.ReadKey();
-            }
-            else if(opcion == 3)
-            {
-                Console.WriteLine("Escribe tu nombre: ");
-                string nombre = Console.ReadLine();
-                Console.WriteLine("Tu nombre es " + nombre);
-                Console.WriteLine("Saliendo... Press any key...");
-                Console.ReadKey();
-            }
-            else if (opcion == 4)
-            {
-                // Hacer una suma con dos números que ingrese el usuario.
-                Console.Write("Escribe un número: ");
-                int numero1 = Convert.ToInt16(Console.ReadLine());
-                Console.Write("Escribe un número: ");
-                int numero2 = Convert.ToInt16(Console.ReadLine());
-                int suma = numero1 + numero2;
-                Console.WriteLine("La suma de " + numero1 + " + " + numero2 + " = " + suma);
-                Console.WriteLine("Saliendo... Press any key...");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("Saliendo... Press any key...");
-                Console.ReadKey();
-            }
+            } while (opcion == 5);
             Console.ReadKey();
         }
     }
